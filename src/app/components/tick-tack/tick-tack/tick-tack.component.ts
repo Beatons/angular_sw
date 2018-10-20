f254613119973f6012e84d9389677f8f7c2f3e22
Alex Dik <mail@alexdik.de>
2018-10-20 21:16:28 +0200

    Change tick tack component indentation

--- src/app/components/tick-tack/tick-tack/tick-tack.component.ts
+++ src/app/components/tick-tack/tick-tack/tick-tack.component.ts
@@ -4,24 +4,25 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'tick-tack',
-    templateUrl: './tick-tack.component.html',
-    styleUrls: ['./tick-tack.component.scss']
+  // tslint:disable-next-line:component-selector
+  selector: 'tick-tack',
+  templateUrl: './tick-tack.component.html',
+  styleUrls: ['./tick-tack.component.scss'],
 })
 
 export class TickTackComponent implements OnInit {
-    array: Observable<any>;
+  array: Observable<any>;
 
-    constructor(
-        private afs: AngularFirestore,
-        private afa: AngularFireAuth,
-    ) { }
+  constructor(
+    private afs: AngularFirestore,
+    private afa: AngularFireAuth,
+  ) {
+  }
 
-    ngOnInit(): void {
-        this.array = this.afs.collection(`games`, ref => {
-           const q: Query = ref;
-            return q;
-        }).valueChanges();
-    }
+  ngOnInit(): void {
+    this.array = this.afs.collection(`games`, ref => {
+      const q: Query = ref;
+      return q;
+    }).valueChanges();
+  }
 }
